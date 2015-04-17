6f2899de37b0708b0279c6ba6110fbcb08ce15c9
Myrat Arazgeldiev <s901@tmm-sapr.org>
2015-04-17 14:47:27 +0300

    Base structure

---
+++ app/src/main/java/com/homedev/myrat/remindme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.homedev.myrat.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by myrat on 17.04.15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
